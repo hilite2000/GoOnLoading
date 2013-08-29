d0f00144861c85953fae04157ed5debfd19eef70
hilite2000 <dearzpfree@hotmail.com>
2013-08-29 11:03:19 +0800

    init

---
+++ MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace GoOnLoading
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnBeginLoad = new System.Windows.Forms.Button();
+            this.btnStopLoad = new System.Windows.Forms.Button();
+            this.tbMsg = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudThreadCount = new System.Windows.Forms.NumericUpDown();
+            this.nudVisitDelay = new System.Windows.Forms.NumericUpDown();
+            this.lblOKCount = new System.Windows.Forms.Label();
+            this.lblFailCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudTimeout = new System.Windows.Forms.NumericUpDown();
+            this.cbUrl = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThreadCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudVisitDelay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 200;
+            // 
+            // btnBeginLoad
+            // 
+            this.btnBeginLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBeginLoad.Location = new System.Drawing.Point(489, 11);
+            this.btnBeginLoad.Name = "btnBeginLoad";
+            this.btnBeginLoad.Size = new System.Drawing.Size(73, 23);
+            this.btnBeginLoad.TabIndex = 1;
+            this.btnBeginLoad.Text = "开始";
+            this.btnBeginLoad.UseVisualStyleBackColor = true;
+            this.btnBeginLoad.Click += new System.EventHandler(this.btnBeginLoad_Click);
+            // 
+            // btnStopLoad
+            // 
+            this.btnStopLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStopLoad.Location = new System.Drawing.Point(568, 11);
+            this.btnStopLoad.Name = "btnStopLoad";
+            this.btnStopLoad.Size = new System.Drawing.Size(73, 23);
+            this.btnStopLoad.TabIndex = 1;
+            this.btnStopLoad.Text = "停止";
+            this.btnStopLoad.UseVisualStyleBackColor = true;
+            this.btnStopLoad.Click += new System.EventHandler(this.btnStopLoad_Click);
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMsg.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.tbMsg.Location = new System.Drawing.Point(5, 94);
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(812, 322);
+            this.tbMsg.TabIndex = 2;
+            this.tbMsg.Text = "";
+            this.tbMsg.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "访问线程数：";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(659, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "访问间隔ms：";
+            // 
+            // nudThreadCount
+            // 
+            this.nudThreadCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudThreadCount.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.nudThreadCount.Location = new System.Drawing.Point(748, 8);
+            this.nudThreadCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudThreadCount.Name = "nudThreadCount";
+            this.nudThreadCount.Size = new System.Drawing.Size(57, 21);
+            this.nudThreadCount.TabIndex = 4;
+            this.nudThreadCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudVisitDelay
+            // 
+            this.nudVisitDelay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudVisitDelay.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.nudVisitDelay.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudVisitDelay.Location = new System.Drawing.Point(748, 35);
+            this.nudVisitDelay.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.nudVisitDelay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudVisitDelay.Name = "nudVisitDelay";
+            this.nudVisitDelay.Size = new System.Drawing.Size(57, 21);
+            this.nudVisitDelay.TabIndex = 4;
+            this.nudVisitDelay.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // lblOKCount
+            // 
+            this.lblOKCount.AutoSize = true;
+            this.lblOKCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblOKCount.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblOKCount.Location = new System.Drawing.Point(66, 35);
+            this.lblOKCount.Name = "lblOKCount";
+            this.lblOKCount.Size = new System.Drawing.Size(152, 27);
+            this.lblOKCount.TabIndex = 5;
+            this.lblOKCount.Text = "lblOKCount";
+            // 
+            // lblFailCount
+            // 
+            this.lblFailCount.AutoSize = true;
+            this.lblFailCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblFailCount.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblFailCount.Location = new System.Drawing.Point(66, 64);
+            this.lblFailCount.Name = "lblFailCount";
+            this.lblFailCount.Size = new System.Drawing.Size(180, 27);
+            this.lblFailCount.TabIndex = 6;
+            this.lblFailCount.Text = "lblFailCount";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(659, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "连接超时ms：";
+            // 
+            // nudTimeout
+            // 
+            this.nudTimeout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudTimeout.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.nudTimeout.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudTimeout.Location = new System.Drawing.Point(748, 62);
+            this.nudTimeout.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.nudTimeout.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.nudTimeout.Name = "nudTimeout";
+            this.nudTimeout.Size = new System.Drawing.Size(57, 21);
+            this.nudTimeout.TabIndex = 4;
+            this.nudTimeout.Value = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            // 
+            // cbUrl
+            // 
+            this.cbUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbUrl.DropDownHeight = 300;
+            this.cbUrl.DropDownWidth = 300;
+            this.cbUrl.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cbUrl.FormattingEnabled = true;
+            this.cbUrl.IntegralHeight = false;
+            this.cbUrl.Location = new System.Drawing.Point(5, 9);
+            this.cbUrl.Name = "cbUrl";
+            this.cbUrl.Size = new System.Drawing.Size(478, 24);
+            this.cbUrl.TabIndex = 7;
+            this.cbUrl.Leave += new System.EventHandler(this.cbUrl_Leave);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 417);
+            this.Controls.Add(this.cbUrl);
+            this.Controls.Add(this.lblFailCount);
+            this.Controls.Add(this.lblOKCount);
+            this.Controls.Add(this.nudTimeout);
+            this.Controls.Add(this.nudVisitDelay);
+            this.Controls.Add(this.nudThreadCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.btnStopLoad);
+            this.Controls.Add(this.btnBeginLoad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "持续加载";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudThreadCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudVisitDelay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnBeginLoad;
+        private System.Windows.Forms.Button btnStopLoad;
+        private System.Windows.Forms.RichTextBox tbMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudThreadCount;
+        private System.Windows.Forms.NumericUpDown nudVisitDelay;
+        private System.Windows.Forms.Label lblOKCount;
+        private System.Windows.Forms.Label lblFailCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudTimeout;
+        private System.Windows.Forms.ComboBox cbUrl;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GoOnLoading
+{
+    public partial class MainForm : Form
+    {
+        private string mUrlFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "url.txt");
+
+        /// <summary>
+        /// 要访问的目标地址
+        /// </summary>
+        private Uri mTargetUri;
+
+        System.Threading.SynchronizationContext mSyncContext;
+
+        private bool mIsRunning;
+
+        /// <summary>
+        /// 在运行中
+        /// </summary>
+        public bool IsRunning
+        {
+            get { return mIsRunning; }
+            set
+            {
+                mIsRunning = value;
+
+                btnBeginLoad.Enabled = !this.mIsRunning;
+                btnStopLoad.Enabled = this.mIsRunning;
+
+                Application.DoEvents();
+            }
+        }
+
+
+        private int mOkCount = 0;
+        private int mFailCount = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.mSyncContext = System.Threading.SynchronizationContext.Current;
+
+            this.IsRunning = false;
+            this.mOkCount = this.mFailCount = 0;
+            RefreshUI();
+        }
+
+        private void cbUrl_Leave(object sender, EventArgs e)
+        {
+            string oldUrl = cbUrl.Text;
+
+            if (cbUrl.Text.StartsWith("http://", StringComparison.CurrentCultureIgnoreCase) == false)
+                cbUrl.Text = "http://" + cbUrl.Text;
+
+            try
+            {
+                this.mTargetUri = new Uri(cbUrl.Text);
+                cbUrl.Text = this.mTargetUri.AbsoluteUri;
+
+                List<string> urlList = new List<string>();
+                foreach (string item in cbUrl.Items) urlList.Add(item);
+                cbUrl.Items.Clear();
+                cbUrl.Items.AddRange(urlList.Distinct().ToArray());
+
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("不正确的URL格式", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cbUrl.Text = oldUrl;
+            }
+        }
+
+        private void btnBeginLoad_Click(object sender, EventArgs e)
+        {
+            if (this.IsRunning == true) return;
+
+            this.mOkCount = this.mFailCount = 0;
+            this.IsRunning = !this.IsRunning;
+            RefreshUI();
+
+
+            for (int idx = 0; idx < nudThreadCount.Value; idx++)
+            {
+                System.Threading.Thread thd = new System.Threading.Thread(new System.Threading.ThreadStart(VisitURL));
+                thd.IsBackground = true;
+                thd.Name = "v" + idx.ToString("000");
+                thd.Start();
+            }
+        }
+
+        private void btnStopLoad_Click(object sender, EventArgs e)
+        {
+            if (this.IsRunning == false) return;
+
+            this.IsRunning = !this.IsRunning;
+
+            RefreshUI();
+        }
+
+        private void VisitURL()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            while (this.IsRunning)
+            {
+                long resultLength = 0;
+                string msg = "";
+                Color msgColor = Color.Black;
+
+                try
+                {
+                    sw.Restart();
+
+                    //System.Net.WebClient wc = new System.Net.WebClient();
+                    //string result = wc.DownloadString(this.mTargetUri);
+                    //resultLength = result.Length;
+
+                    HttpWebRequest request = HttpWebRequest.Create(this.mTargetUri) as HttpWebRequest;
+                    request.Timeout = Convert.ToInt32(nudTimeout.Value);
+                    HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                    resultLength = response.ContentLength;
+                    response.Close();
+
+                    msg = "耗时ms：" + sw.ElapsedMilliseconds.ToString("00000") + "\t内容长度：" + resultLength;
+
+                    System.Threading.Interlocked.Increment(ref this.mOkCount);
+                }
+                catch (Exception ex)
+                {
+                    msg = "耗时ms：" + sw.ElapsedMilliseconds.ToString("00000") + "\t";
+
+                    Exception curEx = ex;
+                    do
+                    {
+                        msg += curEx.Message + "\t";
+                        curEx = curEx.InnerException;
+                    }
+                    while (curEx != null);
+
+                    Interlocked.Increment(ref this.mFailCount);
+                    msgColor = Color.Red;
+                }
+                finally
+                {
+                    sw.Stop();
+                }
+
+                RefreshUI();
+                ShowMsg(Thread.CurrentThread.ManagedThreadId, msg, msgColor);
+
+                Thread.Sleep(Convert.ToInt32(this.nudVisitDelay.Value));
+            }
+        }
+
+        private void RefreshUI()
+        {
+            if (tbMsg.InvokeRequired)
+            {
+                tbMsg.Invoke(new Action(RefreshUI));
+            }
+            else
+            {
+                lblOKCount.Text = "成功：" + this.mOkCount;
+                lblFailCount.Text = "失败：" + this.mFailCount;
+            }
+        }
+
+        private void ShowMsg(int threadId, string msg, Color color)
+        {
+            if (tbMsg.InvokeRequired)
+            {
+                tbMsg.Invoke(new Action<int, string, Color>(ShowMsg), threadId, msg, color);
+            }
+            else
+            {
+                int orgLength = tbMsg.Text.Length;
+
+                string result = "[" + DateTime.Now.ToString("hh:mm:ss.fff") + "]";
+                result += "[" + threadId + "]  ";
+                result += msg;
+                tbMsg.AppendText(result);
+
+                tbMsg.Select(orgLength, result.Length);
+                tbMsg.SelectionColor = color;
+                tbMsg.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.IsRunning = false;
+
+            List<string> urlList = new List<string>();
+            foreach (string item in cbUrl.Items)
+                urlList.Add(item);
+
+            File.WriteAllLines(this.mUrlFileName, urlList.Distinct().ToArray());
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(this.mUrlFileName))
+            {
+                string[] urlList = File.ReadAllLines(this.mUrlFileName);
+                cbUrl.Items.AddRange(urlList);
+            }
+        }
+    }
+}
